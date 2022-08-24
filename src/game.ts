37ac4b233e7589b0ab5535139cbe5270ef01aff1
1Magz1 <46316981+1Magz1@users.noreply.github.com>
2022-08-24 21:17:59 +0700

    add zombie in game

--- src/game.ts
+++ src/game.ts
@@ -1,14 +1,68 @@
 import { Rifle, Cooldown } from './modules/rifle';
+import { Zombie } from './modules/zombie';
+import { zombieParams } from './constants';
 
+// Gun
 const gun = new Rifle(new GLTFShape('models/rifle.glb'), new Transform());
 gun.getComponent(Transform).position.set(0.075, -0.5, 0.2);
 gun.getComponent(Transform).rotation = Quaternion.Euler(-5, 0, 0);
 gun.setParent(Attachable.FIRST_PERSON_CAMERA);
 
+// Zombie
+const zombie = new Zombie(
+  new GLTFShape('models/zombie.glb'),
+  new Transform({
+    position: new Vector3(16, 0.933, 16),
+  }),
+);
+
 // Controls
 const input = Input.instance;
-input.subscribe('BUTTON_DOWN', ActionButton.POINTER, true, () => {
+input.subscribe('BUTTON_DOWN', ActionButton.POINTER, true, (event) => {
   if (gun.hasComponent(Cooldown)) return;
 
   gun.playFireAnim();
+
+  if (event.hit?.meshName === 'Zombie_collider') {
+    log(event.hit?.meshName);
+  }
 });
+
+// Intermediate variables
+const Player = Camera.instance;
+const transform = zombie.getComponent(Transform);
+
+class ZombieAttack implements ISystem {
+  // eslint-disable-next-line class-methods-use-this
+  update(dt: number) {
+    // Rotate to face the player
+    const lookAtTarget = new Vector3(
+      Player.position.x,
+      transform.position.y,
+      Player.position.z,
+    );
+
+    const direction = lookAtTarget.subtract(transform.position);
+    transform.rotation = Quaternion.Slerp(
+      transform.rotation,
+      Quaternion.LookRotation(direction),
+      dt * zombieParams.ROT_SPEED,
+    );
+
+    const distance = Vector3.DistanceSquared(
+      transform.position,
+      Player.position,
+    );
+
+    if (distance >= 4) {
+      zombie.walk();
+      const forwardVector = Vector3.Forward().rotate(transform.rotation);
+      const increment = forwardVector.scale(dt * zombieParams.MOVE_SPEED);
+      transform.translate(increment);
+    } else {
+      zombie.attack();
+    }
+  }
+}
+
+engine.addSystem(new ZombieAttack());
---
+++ src/modules/zombie.ts
@@ -0,0 +1,33 @@
+export class Zombie extends Entity {
+  constructor(
+    model: GLTFShape,
+    transform: Transform,
+  ) {
+    super();
+    engine.addEntity(this);
+    this.addComponent(model);
+    this.addComponent(transform);
+
+    this.addComponent(new Animator());
+
+    this.getComponent(Animator).addClip(
+      new AnimationState('Walking', { looping: true }),
+    );
+    this.getComponent(Animator).addClip(
+      new AnimationState('Attacking', { looping: true }),
+    );
+  }
+
+  attack() {
+    this.getComponent(Animator).getClip('Attacking').play();
+  }
+
+  walk() {
+    this.getComponent(Animator).getClip('Walking').play();
+  }
+
+  stopAnimations() {
+    this.getComponent(Animator).getClip('Walking').stop();
+    this.getComponent(Animator).getClip('Attacking').stop();
+  }
+}
